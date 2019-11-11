285b5082abf7d198e8e666732f3a72babcb76b44
Sebastian Garrido <segasi@gmail.com>
2019-11-10 20:29:35 -0600

    Loop para extraer texto de tablas

--- 02_codigo/analisis_reportes_homicidios.R
+++ 02_codigo/analisis_reportes_homicidios.R
@@ -26,3 +26,83 @@ bd <- tibble(datos = NA,
              entidad = NA,
              numero = NA, 
              fecha = NA)
+
+## Loop para extraer texto de tablas ----
+
+for (i in seq_along(dia_mes)) {
+  
+  ## Leer texto en la primera hoja ----
+  texto <- 
+    pdf_text(pdf = str_c("01_datos/reportes/gpo_interinstitucional/reporte_gpo_interinstitucional_", dia_mes[i], "2019.pdf", sep = ""))[[1]]
+  
+  ## Extraer datos de las dos columnas ----
+  tabla_dos_cols <- 
+    as_tibble(texto) %>% 
+    # Separar renglones usando los linebreaks (\n)
+    separate_rows(value, sep = "\\n") %>% 
+    # Quitar espacios en blanco innecesarios y notas al pies
+    mutate(value = str_trim(value),
+           value = str_squish(value),
+           value = str_replace(value, "a1", "a"),
+           value = str_replace(value, "e1", "e"),
+           value = str_replace(value, "í1", "í"),
+           value = str_replace(value, "o1", "o"),
+           value = str_replace(value, "n1", "n"),
+           value = str_replace(value, "s1", "s"),
+           value = str_replace(value, "t1", "t"),
+           value = str_replace(value, "z1", "z"),
+           value = str_replace(value, "\\*", "")) %>% 
+    # Filtrar renglones que no correspondan a una entidad
+    filter(!str_detect(value, "Víctimas repo"),
+           !str_detect(value, "\\(Fisca"),
+           !str_detect(value, "Entidades"),
+           !str_detect(value, "Fuente"),
+           !str_detect(value, "Fosa"),
+           !str_detect(value, "El día"),
+           !str_detect(value, "Dato obtenido"),
+           !str_detect(value, "cuerpos ubicados"),
+           !str_detect(value, "Abril"),
+           !str_detect(value, "Junio"),
+           !str_detect(value, "Septiembre"),
+           !str_detect(value, "verificados"),
+           !str_detect(value, "corresponden")) %>% 
+    # Separar valores correspondientes a dos estados y ponerlo en dos columnas 
+    separate(value, into = c("col_1",  "col_2"), 
+             sep = "(?<=([0-9] ))",
+             remove = T) %>% 
+    # Quitar espacios en blanco innecesarios
+    mutate(col_1 = str_trim(col_1),
+           col_1 = str_squish(col_1),
+           col_2 = str_trim(col_2),
+           col_2 = str_squish(col_2),
+           col_1 = ifelse(is.na(col_1), "dummy", col_1),
+           col_2 = ifelse(is.na(col_2), "dummy", col_2)) %>%
+    # Filtrar renglones en donde col_1 y col_2 tienen el mismo contenido
+    filter(!(col_1 == col_2)) %>%
+    # Imprimir (nomás pa ver qué está pasando :))
+    print(n = Inf)
+  
+  ## Generar tibble con variables separadas -----
+  
+  # Una columna registra el nombre de la entidad entidad y otra para el número de homicidios dolosos del día correspondiente
+  tabla_datos_separados <- 
+    tibble(datos = c(tabla_dos_cols$col_1, tabla_dos_cols$col_2)) %>%
+    # Eliminar renglones donde datos tiene valor "dummy", "" o incluye el patrón "Total"
+    filter(!str_detect(datos, "Total"),
+           datos != "dummy",
+           datos != "") %>% 
+    # Extrar datos de entidad y número de homicidios
+    mutate(entidad = str_replace(datos, " [0-9]*$", ""),
+           numero = str_extract_all(datos, "\\(?[0-9,.]+\\)?")) %>% 
+    # Desanidar número
+    unnest(numero) %>%
+    # Transformar clase de variable numero y generar variable fecha
+    mutate(numero = as.numeric(numero),
+           fecha = make_date(2019, str_sub(string = dia_mes[i], start = 3, end = 4), str_sub(string = dia_mes[i], start = 1, end = 2))) %>% 
+    print(n = Inf)
+  
+  ## Guardar resultado en el tibble creado antes de arrancar el loop
+  bd <- 
+    bind_rows(bd, tabla_datos_separados) %>% 
+    filter(!is.na(datos)) # Eliminar NAs introducidos al crear el tibble
+}
