ecc3a49c9399b92c1f21bc9d61a21977dda77499
Sebastian Garrido <segasi@gmail.com>
2019-11-10 23:31:37 -0600

    Cambiar tipos de letras

--- 02_codigo/paquetes_setup_tema.R
+++ 02_codigo/paquetes_setup_tema.R
@@ -13,15 +13,15 @@ theme_set(theme_gray())
 ### Definir tema de gráficas ----
 tema <-  
   theme_minimal() +
-  theme(text = element_text(family = "Didact Gothic Regular", color = "grey35"),
-        plot.title = element_text(size = 28, face = "bold", margin = margin(10,0,20,0), family = "Trebuchet MS Bold", color = "grey25"),
-        plot.subtitle = element_text(size = 16, face = "bold", colour = "#666666", margin = margin(0, 0, 20, 0), family = "Didact Gothic Regular"),
+  theme(text = element_text(family = "System Font", color = "grey35"),
+        plot.title = element_text(size = 28, face = "bold", margin = margin(10,0,20,0), family = "Lato Bold", color = "grey25"),
+        plot.subtitle = element_text(size = 16, face = "bold", colour = "#666666", margin = margin(0, 0, 20, 0), family = "System Font"),
         plot.caption = element_text(hjust = 0, size = 15),
         panel.grid = element_line(linetype = 2), 
         panel.grid.minor = element_blank(),
         legend.position = "bottom",
-        legend.title = element_text(size = 16, face = "bold", family = "Trebuchet MS Bold"),
-        legend.text = element_text(size = 14, family = "Didact Gothic Regular"),
+        legend.title = element_text(size = 16, face = "bold", family = "Lato Bold"),
+        legend.text = element_text(size = 14, family = "System Font"),
         legend.title.align = 0.5,
-        axis.title = element_text(size = 18, hjust = 1, face = "bold", margin = margin(0,0,0,0), family = "Didact Gothic Regular"),
-        axis.text = element_text(size = 16, face = "bold", family = "Didact Gothic Regular"))
+        axis.title = element_text(size = 18, hjust = 1, face = "bold", margin = margin(0,0,0,0), family = "System Font"),
+        axis.text = element_text(size = 16, face = "bold", family = "System Font"))
