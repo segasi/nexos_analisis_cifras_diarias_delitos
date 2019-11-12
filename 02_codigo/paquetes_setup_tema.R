2b489324f86bcdd3df1afd4392c170fbf2639125
Sebastian Garrido <segasi@gmail.com>
2019-11-12 12:50:33 -0600

    Iniciar script para descargar reportes

--- 02_codigo/paquetes_setup_tema.R
+++ 02_codigo/paquetes_setup_tema.R
@@ -3,7 +3,7 @@ library(pacman)
 p_load(cowplot, downloader, extrafont, forecast, ggrepel, ggtext, janitor, lubridate, pdftools, psych, readxl, scales, seasonal, sweep, tidyquant, tidyverse, timetk, treemapify, zoo) 
 
 # Importart fuentes
-font_import()
+# font_import()
 
 ### Setup general ----
 Sys.setlocale("LC_ALL", "es_ES.UTF-8") 
