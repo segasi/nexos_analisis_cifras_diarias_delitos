59cee3c882cb0a3e4dc8e3228bc0eabeda350730
Sebastian Garrido <segasi@gmail.com>
2019-11-12 12:51:39 -0600

    Generar vector con fechas de reportes

--- 02_codigo/descargar_reportes.R
+++ 02_codigo/descargar_reportes.R
@@ -1,2 +1,13 @@
 ### Cargar paquetes, definir setup y tema de gráficas -----
 source("02_codigo/paquetes_setup_tema.R")
+
+
+### Generar vector con fechas de reportes ----
+dia_mes <- 
+  tibble(fecha = seq(as.Date("2019-04-03"), length = 223, by = "1 day"),
+         dia = str_pad(day(fecha), width = 2, pad = "0"),
+         mes = str_pad(month(fecha), width = 2, pad = "0"),
+         dia_mes = str_c(dia, mes, sep = "")) %>% 
+  select(dia_mes) %>% 
+  pull()
+
