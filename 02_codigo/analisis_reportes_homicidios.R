770394a8fc7cde7f9e4df948cd82d8b4eb7cba18
Sebastian Garrido <segasi@gmail.com>
2019-11-12 12:54:48 -0600

    Reubicar código para descagar reportes en un scriptb independiente

--- 02_codigo/analisis_reportes_homicidios.R
+++ 02_codigo/analisis_reportes_homicidios.R
@@ -1,24 +1,16 @@
 ### Cargar paquetes, definir setup y tema de gráficas -----
 source("02_codigo/paquetes_setup_tema.R")
 
-### Descargar reportes -----
+### Extraer datos de tablas en reportes -----
 
-# Generar vector con fechas de reportes
+## Generar vector con fechas de reportes ----
 dia_mes <- 
-  tibble(fecha = seq(as.Date("2019-04-03"), length = 219, by = "1 day"),
+  tibble(fecha = seq(as.Date("2019-04-03"), length = 223, by = "1 day"),
          dia = str_pad(day(fecha), width = 2, pad = "0"),
          mes = str_pad(month(fecha), width = 2, pad = "0"),
          dia_mes = str_c(dia, mes, sep = "")) %>% 
   select(dia_mes) %>% 
   pull()
-
-# Descargar reportes
-for (i in seq_along(dia_mes)) {
-  download(url = str_c("http://www.informeseguridad.cns.gob.mx/files/homicidios_", dia_mes[i], "2019_v2.pdf", sep = ""), 
-           destfile = str_c("01_datos/reportes/gpo_interinstitucional/reporte_gpo_interinstitucional_", dia_mes[i], "2019.pdf", sep = ""))
-}
-
-### Extraer datos de tablas en reportes -----
 
 ## Generar tibble vacío ----
 bd <- tibble(datos = NA,
