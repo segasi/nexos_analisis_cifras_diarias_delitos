05b4e3ccfa54f883adfd4f11f55618e6b6282954
Sebastian Garrido <segasi@gmail.com>
2019-11-20 22:55:05 -0600

    Ajustar código para descarga de reportes diarios

--- 02_codigo/descargar_reportes.R
+++ 02_codigo/descargar_reportes.R
@@ -4,7 +4,7 @@ source("02_codigo/paquetes_setup_tema.R")
 
 ### Generar vector con fechas de reportes ----
 dia_mes <- 
-  tibble(fecha = seq(as.Date("2019-04-03"), length = 223, by = "1 day"),
+  tibble(fecha = seq(as.Date("2019-04-03"), length = 212, by = "1 day"),
          dia = str_pad(day(fecha), width = 2, pad = "0"),
          mes = str_pad(month(fecha), width = 2, pad = "0"),
          dia_mes = str_c(dia, mes, sep = "")) %>% 
